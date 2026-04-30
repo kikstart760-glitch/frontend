516994242456e3e14a6754be0ec2847e730f30fb
ashis00011 <mondalashis0011@gmail.com>
2026-04-30 20:21:36 +0530

    commit 11

---
+++ src/Api/Axios.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.baseURL,
+  withCredentials: true,
+});
+
+// attach token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
