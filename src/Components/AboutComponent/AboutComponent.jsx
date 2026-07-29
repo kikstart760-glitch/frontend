31e26d7f12063ac88409f980589382625b729670
ashis00011 <mondalashis0011@gmail.com>
2026-07-29 17:16:55 +0530

    error fix

--- src/Components/AboutComponent/AboutComponent.jsx
+++ src/Components/AboutComponent/AboutComponent.jsx
@@ -11,40 +11,36 @@ function AboutComponent() {
     <h2 className="about-heading">About me</h2>
     <div className="about-cover">
       <div className="about-content">
-        <div className="Container">
-          <Row>
-            <Col md={6} className="about-left">
-              <img src={img1} alt="about" />
-            </Col>
-            <Col md={6} className="about-right">
-              <h6>Hello, and welcome!</h6>
+        <div className="about-left">
+          <img src={img1} alt="about" />
+        </div>
+        <div className="about-right">
+          <h6>Hello, and welcome!</h6>
 
-              <div className="about-title">
-                <h6>I am Jacqueline Rogers</h6>
-              </div>
+          <div className="about-title">
+            <h6>I am Jacqueline Rogers</h6>
+          </div>
 
-              <p className="about-text">
-                The Owner and Travel Advisor of Escape Beyond Travel. With a background in education and 
-                a burning desire to create extraordinary experiences, I founded this company with a clear
-                purpose in mind: to offer a gateway to escape the ordinary and venture beyond the boundaries
-                of everyday life.
-              </p>
+          <p className="about-text">
+            The Owner and Travel Advisor of Escape Beyond Travel. With a background in education and 
+            a burning desire to create extraordinary experiences, I founded this company with a clear
+            purpose in mind: to offer a gateway to escape the ordinary and venture beyond the boundaries
+            of everyday life.
+          </p>
 
-              <p className="about-text">
-                As a former educator, I witnessed firsthand the immense value of broadening one's horizons
-                and embracing new perspectives. I believe that travel has the power to ignite our curiosity, 
-                inspire personal growth, and provide a much-needed respite from our demanding lives. That is 
-                why I decided to channel my passion for exploration into creating a travel agency that goes 
-                beyond traditional itineraries and embraces the concept of transformative journeys.
-              </p>
+          <p className="about-text">
+            As a former educator, I witnessed firsthand the immense value of broadening one's horizons
+            and embracing new perspectives. I believe that travel has the power to ignite our curiosity, 
+            inspire personal growth, and provide a much-needed respite from our demanding lives. That is 
+            why I decided to channel my passion for exploration into creating a travel agency that goes 
+            beyond traditional itineraries and embraces the concept of transformative journeys.
+          </p>
 
-              {/* BUTTONS */}
-              <div className="about-buttons">
-                <Button2Component text="Learn More" path="/destinations" />
-                <button className="linkedin-btn">in</button>
-              </div>
-            </Col>
-          </Row>
+          {/* BUTTONS */}
+          <div className="about-buttons">
+            <Button2Component text="Learn More" path="/destinations" />
+            <button className="linkedin-btn">in</button>
+          </div>
         </div>
       </div>
     </div>
