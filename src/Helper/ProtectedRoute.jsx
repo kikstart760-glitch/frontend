5ba47dcebee92c475ece2a0910879da1eecf3946
ashis00011 <mondalashis0011@gmail.com>
2026-08-10 22:00:00 +0530

    error fix 3

--- src/Helper/ProtectedRoute.jsx
+++ src/Helper/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-    const isAuthenticated = localStorage.getItem("token") !== null;
+    const isAuthenticated = localStorage.getItem("accessToken") !== null;
     if (!isAuthenticated) {
         return <Navigate to="/login" replace />;
     }
